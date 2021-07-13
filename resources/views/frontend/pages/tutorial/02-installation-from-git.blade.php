b5ce654c7edcd554b6d6474fb9b6bd75e6626d5d
enan <emoammerfershid@gmail.com>
2021-07-14 01:34:42 +0600

    updated

--- resources/views/frontend/pages/tutorial/02-installation-from-git.blade.php
+++ resources/views/frontend/pages/tutorial/02-installation-from-git.blade.php
@@ -77,24 +77,4 @@ php artisan key:generate
     <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_f28ex302.json" background="transparent" speed="0.5"
         loop autoplay>
     </lottie-player>
-
-    <div id="bottom-area">
-        <div class="prev-next-link">
-            <div class="link-aula" id="prev_class">
-
-            </div>
-            <div class="link-aula" id="next_class">
-
-            </div>
-        </div>
-
-        <div class="classes">
-            <h3>
-                Contents
-            </h3>
-            <ul id="class_index">
-            </ul>
-        </div>
-
-    </div>
 @endsection
