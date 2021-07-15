269ab43f6ef37b15364c8ae5ea6ccbae342c6d91
enan <emoammerfershid@gmail.com>
2021-07-16 00:49:16 +0600

    several Update

--- resources/views/frontend/includes/head.blade.php
+++ resources/views/frontend/includes/head.blade.php
@@ -19,7 +19,7 @@
 <link rel="icon" type="image/png" href="{{ asset('public/img/Photo_1625506961500.jpg') }}">
 <title>Learn Laravel with Enan - {{ $title }}</title>
 <link href="https://fonts.googleapis.com/css?family=Poppins:400,700" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="{{ asset('public/css/styles.css') }}">
+<link rel="stylesheet" type="text/css" href="public/css/styles.css">
 <link rel="stylesheet" type="text/css"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
