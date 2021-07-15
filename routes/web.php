66433d483f3cdcb6225b39e26fc0cf745d34053c
enan <emoammerfershid@gmail.com>
2021-07-16 00:42:57 +0600

    several Update

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Section\SectionOneController;
 use App\Http\Controllers\Section\SectionTwoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', [HomeController::class, 'index']);
 
 // SECTION ONE
