ea00ccb68d711e8e5bc3954a4d10d7cc2560df42
enan <emoammerfershid@gmail.com>
2021-08-30 14:46:52 +0600

    Edited Head file

--- resources/views/frontend/includes/head.blade.php
+++ resources/views/frontend/includes/head.blade.php
@@ -1,3 +1,13 @@
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="{{ $keywords }}">
+<meta name="description" content="{{ $description }}">
+<meta name="author" content="Enan">
+<!-- <meta property="og:image" content="public/img/logomark.min.svg" /> -->
+<meta property="og:image" content="{{ asset('public/img/Photo_1625506961500.jpg') }}" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 <link rel="stylesheet" href="public/css/default.css">
 <script src="public/tools/indice.js"></script>
 <script>
@@ -7,14 +17,6 @@
 <script>
     hljs.initHighlightingOnLoad();
 </script>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="keywords" content="{{ $keywords }}">
-<meta name="description" content="{{ $description }}">
-<meta name="author" content="Enan">
-<!-- <meta property="og:image" content="public/img/logomark.min.svg" /> -->
-<meta property="og:image" content="{{ asset('public/img/Photo_1625506961500.jpg') }}" />
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
 <!--favicon-->
 <link rel="icon" type="image/png" href="{{ asset('public/img/Photo_1625506961500.jpg') }}">
 <title>Learn Laravel with Enan - {{ $title }}</title>
@@ -23,4 +25,3 @@
 <link rel="stylesheet" type="text/css"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
